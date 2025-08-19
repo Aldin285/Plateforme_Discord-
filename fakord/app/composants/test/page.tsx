da3b97059c3bfda5aed7ace5a9b6554a83d2285e
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2025-08-19 23:23:36 +0100

    Correction d'une erreur coté serveur+ fonction ajout user dans la BDD
    L'erreur empêchait la le lancement de l'appli.
    
    Les améliorations à faire pour l'enregistrement d'un nouveau user seront dans le fichier notes.txt

--- fakord/app/composants/test/page.tsx
+++ fakord/app/composants/test/page.tsx
@@ -12,34 +12,50 @@ export default function Test() {
 
     const [users, setUsers]= useState<IUser[]>([])
 
-    useEffect(()=>{
-        async function AddUser() {
-      try {
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [gender, setGender] = useState('');
+
+
+    async function AddUser() {
         // Ajouter un user dans la BDD
-        // const response = await fetch("/api/users",{
-        //     method: "POST",
-        //     body: JSON.stringify({ 
-        //         firstname: 'Lorien',
-        //         lastname: 'Testard',
-        //         rooms: [1, 2, 3],
-        //         info: {
-        //             birthday: '2000-01-01T00:00:00.000Z',
-        //             gender: 'male'
-        //         }
-        //     }),
-        //     headers: {'Content-Type': 'application/json'}
-        // });
-        
+        const response = await fetch("/api/users",{
+            method: "POST",
+            body: JSON.stringify({ 
+                firstname: firstname,
+                lastname: lastname,
+                rooms: [],
+                info: {
+                    birthday: new Date(birthday),
+                    gender: gender
+                }
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
+
+
+
+    async function fetchUsers() {
+       try {
+      
         const response = await fetch("/api/users")
-        const data = await response.json();
+        const data =  await response.json();
+
         console.log("The data: "+data)
         setUsers(data);
+
       } catch (error) {
         console.error("Error fetching users:", error);
+
       } finally {}
     }
 
-    AddUser();
+    useEffect(()=>{
+    
+      fetchUsers();
+    
     },[])
 
      return (<>
@@ -59,6 +75,20 @@ export default function Test() {
         ):
         <p>No current user</p>
         }
+
+        <br/>
+
+        <h1 className="text-5xl" >Ajouter un user </h1>
+          <br/>
+          <div className='flex flex-col gap-4'>
+            <input type='text' onChange={(e)=>{setFirstname(e.target.value)}} className="bg-cyan-100 text-black field-sizing-content w-fit object-contain p-3 rounded-2xl" name="firstname" placeholder='Firstname...'/>
+            <input type='text' onChange={(e)=>{setLastname(e.target.value)}} className="bg-cyan-100 text-black field-sizing-content w-fit object-contain p-3 rounded-2xl" name="lastname" placeholder='Lastname...'/>
+            <input type='date' onChange={(e)=>{setBirthday(e.target.value)}} className="bg-cyan-100 text-black field-sizing-content w-fit object-contain p-3 rounded-2xl" name="birthday" placeholder='Birthday...'/>
+            <input type='text' onChange={(e)=>{setGender(e.target.value)}} className="bg-cyan-100 text-black field-sizing-content w-fit object-contain p-3 rounded-2xl" name="gender" placeholder='Gender...'/>
+          </div>
+            <br/>
+            <br/>
+            <button onClick={AddUser} type="submit" className="bg-green-300 hover:bg-green-400 text-black border-solid rounded-3xl p-2">Créer</button>
     </div>
 
       </>)
--- fakord/index.js
+++ fakord/index.js
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose'
-import User from './app/model/user.js';
 
 // permet de detecter le fichier .env.local
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
 
-
-mongoose.connect(process.env.URI)
-
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.URI)
+    await mongoose.connect(process.env.URI)
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
@@ -18,24 +14,3 @@ const connectDB = async () => {
   }
 };
 export default connectDB;
-
-
-// ajouter un user dans la liste
-const newUser = new User({
-  firstname: "Ada",
-  lastname: "Wong",
-  dateOfCreation: undefined,
-  rooms: [1,5,4],
-  info: {
-    birthday: '1974-01-01',
-    gender: "Female",
-  } 
-});
-// Insert the article in our MongoDB database
-// await newUser.save();
-
-// Pour vérifier si le nouveau user sera présent dans la BDD
-// const findUser = await User.findOne({firstname:"Ada"});
-// console.log("---------------------------------");
-// console.log(findUser);
-// console.log("---------------------------------");
